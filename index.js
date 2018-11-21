d522ecf5e00d1d5be9bd418316134ca8c65258d2
Adrian Schmid <aschmid@tocco.ch>
2018-11-21 15:51:54 +0100

    add slack eventlistener

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+
+var app = express()
+app.use(bodyParser.json())
+
+var  port = 8080
+
 var SlackBot = require("slackbots")
 var base64 = require("base-64")
 var fetch = require("node-fetch")
@@ -7,29 +15,46 @@ var tocco_token = require("./token")
 var stringToEncode = tocco_token.username + ':' + tocco_token.password
 
 var username = tocco_token.username
-console.log('username', username)
 var password = tocco_token.password
+var regex = "#[0-9]{3,6}"
 
-var fetchTaskURL = 'https://www.tocco.ch/nice2/rest/entities/Task?_where=task_nr==67093&paths_=!'
+var link = 'https://www.tocco.ch/nice2/rest/entities/Task/70368'
 
+var bot = new SlackBot({
+  token: tocco_token.token,
+  name: "toccobot"
+})
+
+app.post('/', function(req, res) {
+    console.log(req.body)
+    if (req.body.challenge) {
+      res.send(req.body.challenge)
+     }
+    if (req.body.event) {
+        console.log("event found")
+        var event = req.body.event;
+        if (event.type && event.text && event.channel && event.type === "message") {
+            console.log("real message")
+            var tasknumbers = event.text.match(regex);
+            if (tasknumbers && tasknumbers.length > 0) {
+                console.log("tasknumbers found")
+                for(var i = 0; i < tasknumbers.length; i++) {
+                    getPromise(tasknumbers[i].substring(1), username, password).then(function(taskDisplay) {
+                        bot.postMessage(event.channel, taskDisplay);
+                    })
+                }
+            }
+        }
+    }
+    res.send("test")
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 let sessionId = null;
 const transformResponse = (response) => {
-  console.log('response', response.data[0].display)
-  response.data[0].display
-  // const labels = response.data.map(
-  //   installation => installation.fields.instance.value,
-  //   );
-  //
-  // if (labels.length === 0) {
-  //   return null;
-  // }
-  //
-  // return {
-  //   title: label,
-  //   type: 'labelList',
-  //   data: labels,
-  // };
+  console.log("response", response)
+  return response.data[0].display
 };
 
 const extractAndStoreSessionId = response => {
@@ -43,25 +68,21 @@ const extractAndStoreSessionId = response => {
   return response;
 };
 
- function getPromise (username, password) {
+ function getPromise (tasknumber, username, password) {
   const headers = {
     Accept: 'application/json',
     Authorization: `Basic ${base64.encode(`${username}:${password}`)}`,
   };
-  console.log('headers', headers)
   if (sessionId) {
     headers.cookie = 'nice_auth=' + sessionId;
   }
+  console.log("tasknumber", tasknumber)
+  var fetchTaskURL = `https://www.tocco.ch/nice2/rest/entities/Task?_where=task_nr==${tasknumber}&paths_=!`
+
   return fetch(fetchTaskURL, { method: 'GET', headers })
     .then(response => extractAndStoreSessionId(response))
     .then(response => response.json())
-    .then(response =>
-       transformResponse(response)
-  )
-    .catch(error =>
-    // logError('backofficeAdapter', 'labelList', error, apiConfig),
-   console.log('error', error)
-    );
+    .then(response => transformResponse(response)
+ ).catch(error => console.log('error', error));
  }
 
-getPromise(username, password)
